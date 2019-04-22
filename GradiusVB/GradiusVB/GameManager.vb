cfa5f223361a4886da3b53f9cb1a654a603ddbf6
Pydronia <jackscmail@gmail.com>
2019-04-22 22:00:16 +1000

    Implement terrain collisions and a death animation

--- GradiusVB/GradiusVB/GameManager.vb
+++ GradiusVB/GradiusVB/GameManager.vb
@@ -20,22 +20,48 @@ Public Enum AnimationFrame
 	Down
 End Enum
 
-' Class for storing the representation of maps
+' Class for storing the representation of maps and terrain
 Public Class Map
 
 	Const mapMoveSpeed As Integer = 64
 
 	Public control As Image
 
+	Public collisionMap As Boolean(,)
+
 	Public length As Integer
 	Public position As Double
 	Public movementSpeed As Double
 
-	Public Sub New(ByVal control As Image)
+	Public Sub New(ByVal control As Image, ByVal collisionImage As BitmapImage)
 		Me.control = control
 		Me.length = control.Width
 		Me.position = 0
 		Me.movementSpeed = mapMoveSpeed
+
+		Canvas.SetZIndex(Me.control, 2)
+
+		' generating collision array
+		' pixel data generation
+		Dim stride As Integer = collisionImage.PixelWidth * collisionImage.Format.BitsPerPixel / 8
+		Dim pixelArray((collisionImage.PixelHeight * stride) - 1) As Byte
+		collisionImage.CopyPixels(pixelArray, stride, 0)
+
+		collisionMap = New Boolean(collisionImage.PixelHeight - 1, collisionImage.PixelWidth - 1) {}
+
+		' collisionmap generation (boolean array)
+		Dim counter As Integer = 0
+		For i = 0 To collisionImage.PixelHeight - 1
+			For j = 0 To collisionImage.PixelWidth - 1
+				If CInt(pixelArray(counter)) > 0 Then
+					collisionMap(i, j) = False
+				Else
+					collisionMap(i, j) = True
+				End If
+				counter = counter + 4
+			Next j
+		Next i
+
 	End Sub
 
 	Public Sub ui_updateMapPosition()
@@ -55,14 +81,24 @@ Public Class Entity
 	Public currentAnimationFrame As AnimationFrame
 	Public animationFrames As BitmapImage()
 
+	Public deathFrameNum As Integer
+	Public deathAnimationFrames As BitmapImage()
+	Public isDying As Boolean = False
+	Public deathAnimationDelay As Integer
+	Public deathAnimationDelayInterval As DateTime
+	Public deathAnimationDelayStart As DateTime
+
+
 	Public type As String
 	Public control As Image
 
-	Public Sub New(ByVal type As String, ByVal control As Image, ByVal position As Point, ByVal movementSpeed As Double)
+	Public Sub New(ByVal type As String, ByVal control As Image, ByVal position As Point, ByVal movementSpeed As Double, ByVal deathDelay As Integer)
 		Me.type = type
 		Me.control = control
 		Me.position = position
 		Me.movementSpeed = movementSpeed
+		Me.deathAnimationDelay = deathDelay
+		Me.deathFrameNum = -1
 		Me.hitBox = New Rect(position.X, position.Y, control.Width, control.Height)
 
 		currentAnimationFrame = AnimationFrame.Neutral
@@ -82,7 +118,16 @@ Public Class Entity
 	End Sub
 
 	Public Sub ui_updateFrame()
-		control.Source = animationFrames(currentAnimationFrame)
+		If Me.isDying Then
+			If deathFrameNum >= deathAnimationFrames.Length Then
+				control.Source = Nothing
+			Else
+				control.Source = deathAnimationFrames(deathFrameNum)
+			End If
+		Else
+			control.Source = animationFrames(currentAnimationFrame)
+		End If
+
 	End Sub
 End Class
 
@@ -90,15 +135,32 @@ End Class
 Public Class VicViper
 	Inherits Entity
 
-	Const startingSpeed As Integer = 128
-	Const startingPosX As Double = 50
-	Const startingPosY As Double = 200
+	Public Const startingSpeed As Integer = 128
+	Public Const startingPosX As Double = 50
+	Public Const startingPosY As Double = 200
+	Public Const vicResetDelay As Integer = 2000
+
+	Private Const vicDeathDelayTime As Integer = 250
+
 
 	Public directionKeys As Directions
 
 	Public Sub New(ByVal control As Image)
-		MyBase.New("vic", control, New Point(startingPosX, startingPosY), startingSpeed)
-		animationFrames = New BitmapImage(2) {GameManager.makeNewBitmap("/Images/vicViper.png"), GameManager.makeNewBitmap("/Images/vicViper_Up.png"), GameManager.makeNewBitmap("/Images/vicViper_Down.png")}
+		MyBase.New("vic", control, New Point(startingPosX, startingPosY), startingSpeed, vicDeathDelayTime)
+
+		Canvas.SetZIndex(Me.control, 0)
+
+		animationFrames = New BitmapImage(2) {GameManager.makeNewBitmapImage("/Images/vicViper.png"), GameManager.makeNewBitmapImage("/Images/vicViper_Up.png"), GameManager.makeNewBitmapImage("/Images/vicViper_Down.png")}
+		deathAnimationFrames = New BitmapImage(3) {GameManager.makeNewBitmapImage("/Images/vicViper_Death_0.png"), GameManager.makeNewBitmapImage("/Images/vicViper_Death_1.png"), GameManager.makeNewBitmapImage("/Images/vicViper_Death_2.png"), GameManager.makeNewBitmapImage("/Images/vicViper_Death_3.png")}
+	End Sub
+
+	Public Sub resetState()
+		isDying = False
+		position = New Point(startingPosX, startingPosY)
+		movementDirection = New Vector(0, 0)
+		movementSpeed = startingSpeed
+		currentAnimationFrame = AnimationFrame.Neutral
+		deathFrameNum = -1
 	End Sub
 
 End Class
@@ -131,6 +193,8 @@ Public Class GameManager
 
 	Private vicAnimationTicking As Boolean
 	Private vicAnimationDelayStart As DateTime
+
+	Private shallReset As Boolean = False
 
 
 #End Region
@@ -191,7 +255,7 @@ Public Class GameManager
 	End Sub
 #End Region
 
-#Region "Initialisation Routines"
+#Region "Initialisation/Reset Routines"
 
 	' setup before game window created
 	Private Sub preSetup()
@@ -207,7 +271,7 @@ Public Class GameManager
 		vicViper.ui_updatePosition()
 		gw.gameField.Children.Add(vicViper.control)
 
-		map = New Map(makeNewSprite("/Images/map.png"))
+		map = New Map(makeNewSprite("/Images/map.png"), makeNewBitmapImage("/Images/collisionMap.png"))
 		map.ui_updateMapPosition()
 		gw.gameField.Children.Add(map.control)
 
@@ -219,6 +283,16 @@ Public Class GameManager
 		AddHandler gameTimer.Elapsed, AddressOf gameLoop
 		gameTimer.AutoReset = False
 		gameTimer.Enabled = True
+	End Sub
+
+	' Reset for when you loose a life
+	Private Sub reset()
+		Canvas.SetZIndex(vicViper.control, 0)
+		shallReset = False
+
+		vicViper.resetState()
+
+		map.position = 0
 	End Sub
 #End Region
 
@@ -232,8 +306,11 @@ Public Class GameManager
 		previousTime = currentTime
 
 		' Processing
-		checkCollisions()
-		moveEntity(vicViper, deltaLoopTime)
+		If checkCollisions() Then
+			gw.Dispatcher.BeginInvoke(New InvokeDelegate(AddressOf destroyVicViper))
+		ElseIf vicViper.isDying = False Then
+			moveEntity(vicViper, deltaLoopTime)
+		End If
 		updateVicAnimationFrame()
 		moveMap(deltaLoopTime)
 
@@ -247,92 +324,178 @@ Public Class GameManager
 #Region "Processing Routines"
 
 	' Check the collisions for the vicviper/entities
-	Private Sub checkCollisions()
+	Private Function checkCollisions()
+		Dim hasDied As Boolean
+		hasDied = False
+		If Not vicViper.isDying Then
+			checkBoundaries()
+			hasDied = checkTerrain(vicViper)
+		End If
+		Return hasDied
+	End Function
+
+	Private Sub checkBoundaries()
 		Dim previous As Directions
 		previous = vicViper.freeDirections
 
-		vicViper.freeDirections.up = vicViper.hitBox.Top > 0
-		vicViper.freeDirections.left = vicViper.hitBox.Left > 0
-		vicViper.freeDirections.down = vicViper.hitBox.Bottom < (gameHeight * scaleFactor) - 1
-		vicViper.freeDirections.right = vicViper.hitBox.Right < (gameWidth * scaleFactor) - 1
+		vicViper.freeDirections.up = vicViper.hitBox.Top > 4
+		vicViper.freeDirections.left = vicViper.hitBox.Left > 4
+		vicViper.freeDirections.down = vicViper.hitBox.Bottom < (gameHeight * scaleFactor) - 5
+		vicViper.freeDirections.right = vicViper.hitBox.Right < (gameWidth * scaleFactor) - 5
 
 		If previous <> vicViper.freeDirections Then
 			updateVicVector()
 		End If
 	End Sub
 
+	Private Function checkTerrain(ByVal entity As Entity)
+		Dim collided As Boolean
+		collided = False
+
+		Dim topLeftCoord As Point
+		topLeftCoord = New Point(CInt(Int((entity.hitBox.X - map.position) / (scaleFactor * 2))), CInt(Int((entity.hitBox.Y) / (scaleFactor * 2))))
+		Dim bottomRightCoord As Point
+		bottomRightCoord = New Point(CInt(Int((entity.hitBox.Right - map.position) / (scaleFactor * 2))), CInt(Int((entity.hitBox.Bottom) / (scaleFactor * 2))))
+
+		Dim i As Integer = topLeftCoord.Y
+		Do Until collided Or i > bottomRightCoord.Y
+			Dim j As Integer = topLeftCoord.X
+			Do Until collided Or j > bottomRightCoord.X
+				If map.collisionMap(i, j) Then
+					collided = True
+				End If
+				j = j + 1
+			Loop
+			i = i + 1
+		Loop
+
+		Return collided
+	End Function
+
 	' move the specified entity and update vicViper animation frame if applicable
 	Private Sub moveEntity(ByVal entity As Entity, ByVal delta As TimeSpan)
 		Dim distance = (delta.TotalMilliseconds / 1000) * entity.movementSpeed
-		entity.position = Vector.Add(entity.movementDirection * distance, entity.position)
+		Dim newPosition As Point
+		newPosition = Vector.Add(entity.movementDirection * distance, entity.position)
+
+		' fix for possible boundary errors
+		If newPosition.X < 0 Then
+			newPosition.X = 0
+		ElseIf newPosition.X > (gameWidth * scaleFactor) - 1 Then
+			newPosition.X = (gameWidth * scaleFactor) - 1
+		End If
+		If newPosition.Y < 0 Then
+			newPosition.Y = 0
+		ElseIf newPosition.Y > (gameHeight * scaleFactor) - 1 Then
+			newPosition.Y = (gameHeight * scaleFactor) - 1
+		End If
+
+		entity.position = newPosition
 	End Sub
 
 	Private Sub moveMap(ByVal delta As TimeSpan)
 		map.position = map.position - ((delta.TotalMilliseconds / 1000) * map.movementSpeed)
 
-		If map.position <= (-3328 * scaleFactor) Then
+		If map.position <= (-map.length + (gameWidth * scaleFactor)) Then
 			map.position = 0
 		End If
 	End Sub
 
+	' update the animation frame of the vic viper
 	Private Sub updateVicAnimationFrame()
-		If vicAnimationTicking And (DateTime.Now - vicAnimationDelayStart).TotalMilliseconds >= vicAnimationDelay Then
-			vicAnimationTicking = False
-			If vicViper.movementDirection.Y > 0 Then
-				vicViper.currentAnimationFrame = AnimationFrame.Down
-			ElseIf vicViper.movementDirection.Y < 0 Then
-				vicViper.currentAnimationFrame = AnimationFrame.Up
+		If Not vicViper.isDying Then
+			' normal updating
+			If vicAnimationTicking And (DateTime.Now - vicAnimationDelayStart).TotalMilliseconds >= vicAnimationDelay Then
+				vicAnimationTicking = False
+				If vicViper.movementDirection.Y > 0 Then
+					vicViper.currentAnimationFrame = AnimationFrame.Down
+				ElseIf vicViper.movementDirection.Y < 0 Then
+					vicViper.currentAnimationFrame = AnimationFrame.Up
+				End If
+			End If
+		Else
+			' play death animation
+			If (DateTime.Now - vicViper.deathAnimationDelayInterval).TotalMilliseconds >= vicViper.deathAnimationDelay Then
+				vicViper.deathAnimationDelayInterval = DateTime.Now
+				vicViper.deathFrameNum = vicViper.deathFrameNum + 1
+			End If
+
+			' check if should reset
+			If (DateTime.Now - vicViper.deathAnimationDelayStart).TotalMilliseconds >= vicViper.vicResetDelay Then
+				shallReset = True
 			End If
 		End If
 	End Sub
 
-	' Update the vicViper's movement vector and animation frame
+	' Update the vicViper's movement vector and start animation frame delay
 	Private Sub updateVicVector()
 		Dim previous As Vector
 		previous = vicViper.movementDirection
-		' calculate vector
-		Dim newVector As Vector
-		newVector = New Vector()
-		If (vicViper.directionKeys.up And vicViper.directionKeys.down) Or Not (vicViper.directionKeys.up Or vicViper.directionKeys.down) Or (vicViper.directionKeys.up And Not vicViper.freeDirections.up) Or (vicViper.directionKeys.down And Not vicViper.freeDirections.down) Then
-			newVector.Y = 0
-		ElseIf vicViper.directionKeys.up Then
-			newVector.Y = -1
-		ElseIf vicViper.directionKeys.down Then
-			newVector.Y = 1
+
+		If Not vicViper.isDying Then
+			' calculate vector
+			Dim newVector As Vector
+			newVector = New Vector()
+			If (vicViper.directionKeys.up And vicViper.directionKeys.down) Or Not (vicViper.directionKeys.up Or vicViper.directionKeys.down) Or (vicViper.directionKeys.up And Not vicViper.freeDirections.up) Or (vicViper.directionKeys.down And Not vicViper.freeDirections.down) Then
+				newVector.Y = 0
+			ElseIf vicViper.directionKeys.up Then
+				newVector.Y = -1
+			ElseIf vicViper.directionKeys.down Then
+				newVector.Y = 1
+			End If
+
+			If (vicViper.directionKeys.left And vicViper.directionKeys.right) Or Not (vicViper.directionKeys.left Or vicViper.directionKeys.right) Or (vicViper.directionKeys.left And Not vicViper.freeDirections.left) Or (vicViper.directionKeys.right And Not vicViper.freeDirections.right) Then
+				newVector.X = 0
+			ElseIf vicViper.directionKeys.left Then
+				newVector.X = -1
+			ElseIf vicViper.directionKeys.right Then
+				newVector.X = 1
+			End If
+
+			'If newVector.Length <> 0 Then
+			'	newVector.Normalize()
+			'End If
+
+			vicViper.movementDirection = newVector
 		End If
-
-		If (vicViper.directionKeys.left And vicViper.directionKeys.right) Or Not (vicViper.directionKeys.left Or vicViper.directionKeys.right) Or (vicViper.directionKeys.left And Not vicViper.freeDirections.left) Or (vicViper.directionKeys.right And Not vicViper.freeDirections.right) Then
-			newVector.X = 0
-		ElseIf vicViper.directionKeys.left Then
-			newVector.X = -1
-		ElseIf vicViper.directionKeys.right Then
-			newVector.X = 1
-		End If
-
-		'If newVector.Length <> 0 Then
-		'	newVector.Normalize()
-		'End If
-
-		vicViper.movementDirection = newVector
 
 		' animation timer start
-		If newVector.Y = 0 Then
+		If vicViper.movementDirection.Y = 0 Then
 			vicAnimationTicking = False
 			vicViper.currentAnimationFrame = AnimationFrame.Neutral
-		ElseIf previous.Y <> newVector.Y Then
+		ElseIf previous.Y <> vicViper.movementDirection.Y Then
 			vicAnimationTicking = True
 			vicAnimationDelayStart = DateTime.Now
 		End If
 
 	End Sub
+
+	Private Sub destroyVicViper()
+		vicViper.isDying = True
+		lives = lives - 1
+		Canvas.SetZIndex(vicViper.control, 50)
+		vicViper.deathFrameNum = 0
+		vicViper.deathAnimationDelayInterval = DateTime.Now
+		vicViper.deathAnimationDelayStart = DateTime.Now
+	End Sub
 #End Region
 
 #Region "UI Update Routines"
 	Private Sub updateUI()
-		vicViper.ui_updatePosition()
-		vicViper.ui_updateFrame()
-		map.ui_updateMapPosition()
+		If shallReset Then
+			reset()
+		Else
+			vicViper.ui_updatePosition()
+			vicViper.ui_updateFrame()
+			map.ui_updateMapPosition()
+			updateInterface()
+		End If
 	End Sub
+
+	Private Sub updateInterface()
+		gw.lblLives.Text = getLives()
+	End Sub
+
 #End Region
 
 #Region "Other routines"
@@ -340,7 +503,7 @@ Public Class GameManager
 	Private Function makeNewSprite(ByVal source As String) As Image
 		Dim newControl As New Image()
 		Dim newBitmap As BitmapImage
-		newBitmap = makeNewBitmap(source)
+		newBitmap = makeNewBitmapImage(source)
 		newControl.Source = newBitmap
 		newControl.Width = newBitmap.PixelWidth * scaleFactor
 		newControl.Height = newBitmap.PixelHeight * scaleFactor
@@ -348,11 +511,8 @@ Public Class GameManager
 	End Function
 
 	' helper function to make new image for sprite
-	Public Shared Function makeNewBitmap(ByVal source As String) As BitmapImage
-		Dim newBitmap As New BitmapImage()
-		newBitmap.BeginInit()
-		newBitmap.UriSource = New Uri("pack://application:,,," & source)
-		newBitmap.EndInit()
+	Public Shared Function makeNewBitmapImage(ByVal source As String) As BitmapImage
+		Dim newBitmap As New BitmapImage(New Uri("pack://application:,,," & source))
 		Return newBitmap
 	End Function
 #End Region
